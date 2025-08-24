5e0092ae4292611aa447b1cf053b42d521938f8c
naskar-sukanta <sukanta.naskar@gmail.com>
2025-08-24 13:34:57 +0530

    Initial commit: Hello World Java project

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,9 @@
+// src/main/java/com/example/HelloWorld.java
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from GitHub Actions!");
+        System.out.println("Deployed to self-hosted Linux server!");
+    }
+}
